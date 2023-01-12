5e080ed93ff58325fd7e35cffac7484a9ce3a480
richardbmezzomo <richardmezzomo1@gmail.com>
2023-01-12 17:20:06 -0300

    inicialized js

---
+++ index.js
@@ -0,0 +1,5 @@
+const buttonPlay = document.querySelector('.play')
+const buttonStop = document.querySelector('.stop')
+const buttonPlus = document.querySelector('.plus')
+const buttonMinus = document.querySelector('.minus')
+
