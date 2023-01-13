cca724912fa149d22828c388dd213afe1ad51a0e
richardbmezzomo <richardmezzomo1@gmail.com>
2023-01-13 15:26:31 -0300

    fixed a bug on updateTimerDisplay

--- index.js
+++ index.js
@@ -11,13 +11,18 @@ function resetControls() {
   buttonPlay.classList.remove('hide')
 }
 
+function updateTimerDisplay(minutes, seconds) {
+  minutesDisplay.textContent = String(minutes).padStart(2, "0")
+  secondsDisplay.textContent = String(seconds).padStart(2, "0")
+}
+
 
 function countdown() {
   setTimeout(function(){
     let seconds = Number(secondsDisplay.textContent)
     let minutes = Number(minutesDisplay.textContent)
 
-    secondsDisplay.textContent = "00"
+    updateTimerDisplay(minutes, 0)
 
     if(minutes <= 0) {
       resetControls()
@@ -27,10 +32,10 @@ function countdown() {
     if(seconds <= 0 ) {
       seconds = 3
 
-      minutesDisplay.textContent = String(minutes - 1).padStart(2, "0")
+      --minutes
     }
 
-    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
+    updateTimerDisplay(minutes, String(seconds - 1))
 
     countdown()
   }, 1000)
